0bdf0be82bd214ba93d73eddfee416bff547cd05
J-TKim <68172322+J-TKim@users.noreply.github.com>
2020-09-10 19:36:36 +0900

    두번째 덧셈 프로그램

---
+++ Ch2/Ch2/sum_calc_2.c
@@ -0,0 +1,14 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+int main(void) {
+	int ysalary, msalary;
+
+	printf("연봉을 입력하시오(단위: 만원):");
+	scanf("%d", &ysalary);
+
+	msalary = ysalary / 12;
+	printf("월 수령액(단위: 만원): %d\n", msalary);
+
+	return 0;
+}
